98f5e6d91bb59e6fcd1988f8b1ee111a98c873d9
Oywayten <oywayten@gmail.com>
2023-11-12 21:58:16 +0300

    add schema.sql, data.sql, OrderRepository.java and new logo

---
+++ src/main/java/vitaliy/grab/doners/repository/OrderRepository.java
@@ -0,0 +1,11 @@
+package vitaliy.grab.doners.repository;
+
+import vitaliy.grab.doners.model.Order;
+
+/**
+ * Oywayten 12.11.2023.
+ */
+
+public interface OrderRepository {
+    Order save(Order order);
+}
