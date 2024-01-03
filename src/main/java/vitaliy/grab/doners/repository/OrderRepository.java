a57a6240db65a7161601fb5bca9bc0a77b0225bd
Oywayten <oywayten@gmail.com>
2024-01-03 14:17:47 +0300

    add MongoDB

--- src/main/java/vitaliy/grab/doners/repository/OrderRepository.java
+++ src/main/java/vitaliy/grab/doners/repository/OrderRepository.java
@@ -3,11 +3,9 @@ package vitaliy.grab.doners.repository;
 import org.springframework.data.repository.CrudRepository;
 import vitaliy.grab.doners.model.DonerOrder;
 
-import java.util.UUID;
-
 /**
  * Oywayten 12.11.2023.
  */
 
-public interface OrderRepository extends CrudRepository<DonerOrder, UUID> {
+public interface OrderRepository extends CrudRepository<DonerOrder, String> {
 }
