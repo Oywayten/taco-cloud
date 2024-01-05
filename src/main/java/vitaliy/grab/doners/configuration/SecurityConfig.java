30e2a71815d4a2758c8b7bc9cee43d5b6d47d20a
Oywayten <oywayten@gmail.com>
2024-01-05 10:39:42 +0300

    add storage of user information in memory

--- src/main/java/vitaliy/grab/doners/configuration/SecurityConfig.java
+++ src/main/java/vitaliy/grab/doners/configuration/SecurityConfig.java
@@ -2,8 +2,16 @@ package vitaliy.grab.doners.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Oywayten 05.01.2024.
@@ -15,5 +23,13 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+        List<UserDetails> users = new ArrayList<>();
+        users.add(new User("buzz", encoder.encode("password"), List.of(new SimpleGrantedAuthority("ROLE_USER"))));
+        users.add(new User("woody", encoder.encode("password"), List.of(new SimpleGrantedAuthority("ROLE_USER"))));
+        return new InMemoryUserDetailsManager(users);
     }
 }
